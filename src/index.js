de3413e707b6b6d9db5a11d0c954e7d66f96bbb7
Thomas Perrault <thomas.perrault.pro@gmail.com>
2022-10-06 10:32:34 +0200

    MAJ index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Components
+import App from './components/App';
+
+createRoot(document.getElementById('root')).render(<App />)
+
