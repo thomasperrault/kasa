fe00c8347403d4ae5c4d6d802edf256505ece818
Thomas Perrault <thomas.perrault.pro@gmail.com>
2022-10-13 13:50:36 +0200

    Modification format app apropos 303 logements

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../style/error404.css';
+
+
+const Error404 = () => (
+    <div className="Error404">
+        <h1 className="Error404_code">404</h1>
+        <p className="Error404_message">Oups! La page que vous demandez n'existe pas.</p>
+        <a href="/" className="Error404_link">Retourner sur la page d'accueil</a>
+    </div>
+)
+
+export default Error404
