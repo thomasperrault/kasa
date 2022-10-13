21a81a83293ae0ce2e0d58788f66841be3b7b4ae
Thomas Perrault <thomas.perrault.pro@gmail.com>
2022-10-13 13:47:44 +0200

    Modification format header et footer js > jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import logo_couleur from '../img/logo_couleur.png';
 import '../style/header.css';
+import { Link } from "react-router-dom";
 
 const Header = () => (
     <header className="header"> 
         <img className="logo_couleur" src={logo_couleur} alt="Logo Kasa"></img>   
         <nav className="header_nav">
-            <a href="/" className="header_nav_item" >Accueil</a>
-            <a href="/a-propos" className="header_nav_item" >A Propos</a>
+            <Link to="/" className="header_nav_item" >Accueil</Link>
+            <Link to="/a-propos" className="header_nav_item" >A Propos</Link>
         </nav>
     </header>
 )
